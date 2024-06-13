74bb7ae664b2a623ccddf566dfc75b762a899755
chao_wang <chao_wang@gmail.com>
2024-06-13 18:04:24 +0800

    feat: flx

--- packages/vector-vue/lib/vector-vue.js
+++ packages/vector-vue/lib/vector-vue.js
@@ -1,7 +1,7 @@
 'use strict';
 import Vue from 'vue';
 
-import MyComponent from './MyComponent.vue';
+import MyComponent from '../src/MyComponent.vue';
 export default {
   install: function (Vue) {
     Vue.component('my-component', MyComponent);
