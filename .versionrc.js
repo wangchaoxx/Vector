a059094bab260074a57efe23cc1442cba39ec509
chao_wang <chao_wang@gmail.com>
2024-06-14 17:55:15 +0800

    chore: 添加更新日志

---
+++ .versionrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "types": [
+    { "type": "feat", "section": "Features" },
+    { "type": "fix", "section": "Bug Fixes" },
+    { "type": "chore", "section": "Chores" },
+    { "type": "docs", "section": "Documentation" },
+    { "type": "style", "section": "Styles" },
+    { "type": "refactor", "section": "Code Refactoring" },
+    { "type": "perf", "section": "Performance Improvements" },
+    { "type": "test", "section": "Tests" },
+    { "type": "build", "section": "Build System" },
+    { "type": "ci", "section": "Continuous Integration" }
+  ],
+  "releaseCommitMessageFormat": "chore(release): {{currentTag}}"
+};
