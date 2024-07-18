bf12d34bd7d0869bfb3493c07f22bd5026a8ce77
chao_wang <chao_wang@gmail.com>
2024-07-18 11:11:21 +0800

    feat: add cli config

---
+++ packages/cli/dist/index.mjs
@@ -0,0 +1,12 @@
+function getDefaultExportFromCjs (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+var src = cli;
+function cli() {
+  return "Hello from cli";
+}
+
+const index = /*@__PURE__*/getDefaultExportFromCjs(src);
+
+export { index as default };
