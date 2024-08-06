e3547c5e3cc31fed6eb713077987c9e3d6fbce32
chao_wang <chao_wang@gmail.com>
2024-08-06 16:10:06 +0800

    feat: 添加ts类型文件

--- packages/utils/index.d.ts
+++ packages/utils/index.d.ts
@@ -2,6 +2,14 @@
 
 
 interface log {
+  level: string;
+  record: MessageObject[];
+  maxRecordSize: number;
+  prefixStyle: StyleObject;
+  headingStyle: StyleObject;
+  heading: string;
+  stream: any; // Defaults to process.stderr
+
   success(prefix: string, message: any, ...args: any[]): void;
   info(prefix: string, message: any, ...args: any[]): void;
   warn(prefix: string, message: any, ...args: any[]): void;
